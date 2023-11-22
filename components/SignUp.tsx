fbe907f706f151e8304d982cfc97a02a9c2126b8
Benjtalkshow <chinedubenj@gmail.com>
2023-11-22 11:14:30 -0800

    installed zod

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Slider from './formComponents/Slider';
+import Form from './formComponents/Form';
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+    return (
+        <div>
+            <Form/>
+        </div>
+    )
 }
 
 export default SignUp;
--- components/formComponents/Slider.tsx
+++ components/formComponents/Slider.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import logo from '@/public/svg/logo.svg';
+import bgImage from '@/public/svg/formBg.svg';
+// import '@/styles/slider.css';
 
 const Slider = () => {
   return (
-    <div>Slider</div>
-  )
-}
+    <div style={{ backgroundImage: `url(${bgImage})` }} className='formBg h-screen w-full'>
+      <Image src={logo} alt="Logo" width={100} height={100} />
+    </div>
+  );
+};
 
 export default Slider;
