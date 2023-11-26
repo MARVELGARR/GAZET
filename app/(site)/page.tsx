4bc2409aac5ecd7fa82cfaf455e0a89222d83321
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-11-26 20:31:55 +0100

    removed some data

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,10 +4,6 @@ import RecentInvoice from "@/components/MarvelComponents/dashboard/recentinvoice
 
 export default function Home() {
 
-  const chartData = {
-    labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
-    values: [10, 30, 15, 25, 20],
-  };
 
   return (
     <main className=' h-full w-full flex gap-3 items-center'>
